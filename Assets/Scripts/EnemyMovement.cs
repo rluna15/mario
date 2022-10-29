d241e09c0095a5e4414f1589f52cfe1f005ab005
rluna15 <58538880+rluna15@users.noreply.github.com>
2022-10-29 00:56:47 -0400

    enemy movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D rb;
+    BoxCollider2D bxCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        bxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(moveSpeed, 0);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        moveSpeed = -moveSpeed;
+        FlipEnemy();
+    }
+
+    void FlipEnemy()
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(rb.velocity.x)), 1f);
+    }
+
+}
