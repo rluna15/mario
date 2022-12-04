fb364b8a16339924cd854f3d79d7101495d65db5
rluna <no-replay@github.com>
2022-12-03 22:38:11 -0500

    added brick collision

---
+++ Assets/Scripts/BricksCollision.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BricksCollision : MonoBehaviour
+{
+    [SerializeField] Grid grid;
+    [SerializeField] Tilemap tilemap;
+
+    Vector3 playerPos;
+    Vector3 tilePos;
+    Vector3 posOffset = new Vector3(0, 0.5f, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Bricks")
+        {
+            playerPos = transform.position;
+            tilePos = playerPos + posOffset;
+            
+            Vector3Int position = grid.WorldToCell(tilePos);
+            tilemap.SetTile(position, null);
+        }
+    }
+}
