3564df7eac65b4aad0aca4af84ba184c730a7278
rluna <no-replay@github.com>
2022-12-04 02:41:25 -0500

    mystery box items are created

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -8,26 +8,67 @@ public class Box : MonoBehaviour
     [SerializeField] Object coin;
     [SerializeField] Object growShroom;
     [SerializeField] Object fireFlower;
+    [SerializeField] Sprite disabledSprite;
 
     [Header("Select")]
     [SerializeField] bool isCoin;
+    [SerializeField] bool isMultiHit;
+    [SerializeField] int hitCount;
     [SerializeField] bool isGrowShroom;
     [SerializeField] bool isFireFlower;
 
+    bool isHit = false;
+
+    Object obj;
+
+    SpriteRenderer sp;
+
+    Vector3 position;
+    Vector3 offSet = new Vector3(0, 1f, 0);
+
+    void Start()
+    {
+        sp = GetComponent<SpriteRenderer>();
+        position = transform.position + offSet;
+    } 
+
     public void ShowItem()
     {
-        if(isCoin)
+        if(isCoin && !isHit)
         {
+            isHit = true;
+            sp.sprite = disabledSprite;
+            obj = Instantiate(coin, position, Quaternion.identity);
+            Destroy(obj, 0.3f);
             Debug.Log("Show Coin.");
         }
-        
-        if(isGrowShroom)
+
+        if (isMultiHit && !isHit)
         {
+            hitCount--;
+            obj = Instantiate(coin, position, Quaternion.identity);
+            Destroy(obj, 0.3f);
+
+            if (hitCount < 1)
+            {
+                isHit = true;
+                sp.sprite = disabledSprite;
+            }
+        }
+        
+        if(isGrowShroom && !isHit)
+        {
+            isHit = true;
+            obj = Instantiate(growShroom, position, Quaternion.identity);
+            sp.sprite = disabledSprite;
             Debug.Log("Show GrowShroom.");
         }
 
-        if(isFireFlower)
+        if(isFireFlower && !isHit)
         {
+            isHit = true;
+            obj = Instantiate(fireFlower, position, Quaternion.identity);
+            sp.sprite = disabledSprite;
             Debug.Log("Show FireFlower.");
         }
     }
