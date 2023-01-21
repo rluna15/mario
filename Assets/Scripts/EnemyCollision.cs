b08f77bd220a803f41b5dd0d0bd8115862c1f53d
rluna15 <58538880+rluna15@users.noreply.github.com>
2023-01-21 17:59:22 -0500

    Revert "setting up the enemy death sprite"
    This reverts commit 8344df2630dd084b0741f702d2467a4a6066a64e.

--- Assets/Scripts/EnemyCollision.cs
+++ Assets/Scripts/EnemyCollision.cs
@@ -4,30 +4,19 @@ using UnityEngine;
 
 public class EnemyCollision : MonoBehaviour
 {
-    [Header("Colliders")]
     [SerializeField] BoxCollider2D headCollider;
     [SerializeField] GameObject player;
-
-    [Header("Death Sprite")]
-    [SerializeField] Sprite deathSprite;
-    [SerializeField] GameObject enemyObject;
 
     EnemyMovement enemyMovement;
 
     BoxCollider2D feetCollider;
-    Rigidbody2D rb;
-    Rigidbody2D playerRB;
-    SpriteRenderer sr;
 
     bool isTouching;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         feetCollider = player.GetComponent<BoxCollider2D>();
         enemyMovement = GetComponent<EnemyMovement>();
-        playerRB = GetComponent<Rigidbody2D>();
-        sr = enemyObject.GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -36,9 +25,8 @@ public class EnemyCollision : MonoBehaviour
 
         if (isTouching)
         {
-            rb.bodyType = RigidbodyType2D.Static;
             enemyMovement.enabled = false;
-            sr.sprite = deathSprite;
+            Debug.Log("killed by player");
         }
     }
 }
